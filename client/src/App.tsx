61541a33245f082561c9466078a7cd883d72f352
Chris <chrisnash819@yahoo.com>
2024-11-30 12:47:45 -0800

    added basic version of NewJob and JobDisplay components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import NewJob from './NewJob';
+import JobDisplay from './JobDisplay';
+import './JobDisplay.css';
 
 const App: React.FC = () => {
+    const [jobs, setJobs] = useState<{ jobTitle: string; companyName: string; location: string, notes: string }[]>([]);
+
+    const addJob = (newJob: { jobTitle: string; companyName: string; location: string, notes: string}) => {
+        setJobs([...jobs, newJob]);
+    };
+
+
+
     return (
-        <>
-            <h1>Team Pikachu!</h1>
-        </>
+        <div>
+            <h1>Job List</h1>
+            <NewJob addJob={addJob}/>
+            <div className="job-list">
+                {jobs.map((job, index) => (
+                    <JobDisplay 
+                    key={index}
+                    companyName={job.companyName}
+                    jobTitle={job.jobTitle}
+                    location={job.location}
+                    notes={job.notes}
+                    />
+                ))}
+            </div>
+        </div>
     );
 };
 
